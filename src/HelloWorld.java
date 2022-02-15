4a8994561c5b11a7045f4478bdd0b3df3a38c325
Charles Willcockson <willcockson.charles@gmail.com>
2022-02-15 11:30:13 -0600

    finished the second exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,27 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int MyFavoriteNumber = 69;
+        String MyString = "I love chickens!";
+        System.out.println(MyString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        int x = 4 + 5;
+//        int x = 3;
+//        int y = 4 * x;
+//        int x = 10 / 2;
+//        int y = 2 - x;
+//        System.out.println(y);
+
     }
 }
