cefb8ee9f4ed13aa33dfb900801ea7433a447515
Charles Willcockson <willcockson.charles@gmail.com>
2022-02-16 10:48:57 -0600

    finished string exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,12 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String pinkFloyd = "We don't need no education \nWe don't need no thought control";
+        System.out.println(pinkFloyd);
+        String dubQuotes = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(dubQuotes);
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+        String backslash = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash\\\\\\!";
+        System.out.println(backslash);
+    }
+}
