0e62a21f0d0711c186888b95f0ea5042ac343fb7
Charles Willcockson <willcockson.charles@gmail.com>
2022-02-17 16:33:38 -0600

    Gerald has a cat

---
+++ src/adventureGame.java
@@ -0,0 +1,6 @@
+//public class adventureGame {
+//    public static void main(String[] args) {
+//
+//    }
+//    public static void
+//}
---
+++ src/practiceFromW3.java
@@ -0,0 +1,11 @@
+//   w3resource.com/java-exercises/basic/index.php
+
+
+import java.util.Scanner;
+
+public class practiceFromW3 {
+    public static void main(String[] args) {
+
+//        System.out.println("Hello\nCharles Willcockson");
+    }
+}
