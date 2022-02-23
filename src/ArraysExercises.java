978902e9601b9b523761b172643d01d7ac76afa0
Charles Willcockson <willcockson.charles@gmail.com>
2022-02-23 11:15:23 -0600

    finished java arrays exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,6 +1,28 @@
+import util.Input;
+import java.util.Arrays;
+
 public class ArraysExercises {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(numbers);
+        Person person1 = new Person("Charles");
+        Person person2 = new Person("Nelson");
+        Person person3 = new Person("Andrew");
+        Person[] personas = {person1, person2, person3};
+        for (int i = 0; i < personas.length; i++){
+            System.out.println(personas[i].getName());
+        }
+        Person person4 = new Person("Gerald");
+        personas = addPerson(personas, person4);
+        for (int i = 0; i < personas.length; i++){
+            System.out.println(personas[i].getName());
+        }
+    }
+
+    public static Person[] addPerson(Person[] personas, Person person) {
+        Person[] personas2 = new Person[personas.length + 1];
+        for (int i = 0; i < personas.length; i++){
+            personas2[i] = personas[i];
+        }
+        personas2[personas2.length -1] = person;
+        return personas2;
     }
 }
