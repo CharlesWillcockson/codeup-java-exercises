0fdc70865fcfe30a31be7991e939ddafff67658e
Charles Willcockson <willcockson.charles@gmail.com>
2022-02-24 09:40:15 -0600

    wrote getters and setters for square

---
+++ src/shapes/Square.java
@@ -0,0 +1,20 @@
+package shapes;
+
+public class Square extends Rectangle {
+    private double side;
+
+    public Square (double side) {
+
+    }
+
+    public void setLength() {
+        this.length = length;
+    }
+
+    public void setWidth() {
+        this.width = width;
+    }
+
+
+
+}
