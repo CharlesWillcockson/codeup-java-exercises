6679828ef5c20445e78b740c7917d90918ee227f
Charles Willcockson <willcockson.charles@gmail.com>
2022-02-28 10:10:29 -0600

    chnged rectangle and square

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,35 +1,61 @@
 package shapes;
 
-public class Rectangle {
-    protected double length;
-    protected double width;
+public class Rectangle extends Quadrilateral implements Measurable{
 
-    public Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
+
+    public Rectangle(int length, int width) {
+        super(length, width);
     }
 
-    public double getLength() {
-        return length;
-    }
-
-    public void setLength(double length) {
-        this.length = length;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
-
+    @Override
     public double getPerimeter() {
-        return (2 * this.length) + (2 * this.width);
+        return 0;
     }
 
+    @Override
     public double getArea() {
-        return this.length * this.width;
+        return 0;
+    }
+
+    @Override
+    public void setLength(int length) {
+
+    }
+
+    @Override
+    public void setWidth(int width) {
+
     }
 }
+//    protected double length;
+//    protected double width;
+//
+//    public Rectangle(double length, double width) {
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public double getLength() {
+//        return length;
+//    }
+//
+//    public void setLength(double length) {
+//        this.length = length;
+//    }
+//
+//    public double getWidth() {
+//        return width;
+//    }
+//
+//    public void setWidth(double width) {
+//        this.width = width;
+//    }
+//
+//    public double getPerimeter() {
+//        return (2 * this.length) + (2 * this.width);
+//    }
+//
+//    public double getArea() {
+//        return this.length * this.width;
+//    }
+
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,21 +1,47 @@
 package shapes;
 
-public class Square extends Rectangle {
-    private double side;
+abstract class Square extends Quadrilateral {
 
-    public Square(double side) {
+    public Square(int side){
         super(side, side);
     }
 
-    public double getArea() {
-        double side = super.length;
-        return Math.pow(side, 2);
-    }
-
+    @Override
     public double getPerimeter() {
-        double side = super.length;
-        return 4 * side;
+        return 2 * (length + width);
     }
 
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public void setWidth(int width) {
+        this.width = width;
+    }
 
 }
+//    private double side;
+//
+//    public Square(double side) {
+//        super(side, side);
+//    }
+//
+//    public double getArea() {
+//        double side = super.length;
+//        return Math.pow(side, 2);
+//    }
+//
+//    public double getPerimeter() {
+//        double side = super.length;
+//        return 4 * side;
+//    }
+//
+//
+//}
