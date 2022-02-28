08b79040ca899e4556ec0a733a32440addf8aaca
Charles Willcockson <willcockson.charles@gmail.com>
2022-02-28 15:35:11 -0600

    finished in class exercise

---
+++ src/TheSumOfAllArrays.java
@@ -0,0 +1,19 @@
+public class TheSumOfAllArrays {
+
+
+    public static int sumAll(){
+        int sum = 0;
+        int sumArray[] = {5, 2, 3, 1};
+
+        for (int n : sumArray){
+            sum += n;
+        }
+        return sum;
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(sumAll());
+    }
+}
