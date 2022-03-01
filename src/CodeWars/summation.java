c7d4012864714b233eef77b93da808df80a4e8f6
Charles Willcockson <willcockson.charles@gmail.com>
2022-03-01 14:54:02 -0600

    codewars sumation kata completed

---
+++ src/CodeWars/summation.java
@@ -0,0 +1,12 @@
+package CodeWars;
+
+public class summation {
+
+    public static int summation(int num){
+        int sum = 0;
+        for (int i = 0; i <= num; i++){
+            sum += i;
+        }
+        return sum;
+    }
+}
---
+++ src/grades/Student.java
@@ -0,0 +1,7 @@
+package grades;
+import java.util.ArrayList;
+
+public class Student {
+    public String name;
+    public ArrayList<Integer> grades;
+}
