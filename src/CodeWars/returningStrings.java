b63c500bb1f451c6a409369ae293020e287bbee7
Charles Willcockson <willcockson.charles@gmail.com>
2022-03-01 13:57:36 -0600

    codewars returningStrings complete

---
+++ src/CodeWars/returningStrings.java
@@ -0,0 +1,12 @@
+package CodeWars;
+
+public class returningStrings {
+    public static String greet(String name){
+
+        return "Hello, " + name + " how are you doing today?";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greet("Charles"));
+    }
+}
