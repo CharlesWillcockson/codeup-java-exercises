08d368ce34cc2545cdba6bbae08188ddbba64367
Charles Willcockson <willcockson.charles@gmail.com>
2022-03-02 08:59:00 -0600

    started collections exercise...student grades

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -4,4 +4,22 @@ import java.util.ArrayList;
 public class Student {
     public String name;
     public ArrayList<Integer> grades;
+
+    public String getName(){
+        return name;
+    }
+
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    public double getGradeAverage(){
+        double average = 0.0;
+        int sum = 0;
+        for (int grade : grades){
+            sum += grade;
+        }
+        average = (double) sum / grades.size();
+        return average;
+    }
 }
