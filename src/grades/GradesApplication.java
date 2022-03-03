7875d7ee0b130b0bc72817b6025736e1d61409a3
Charles Willcockson <willcockson.charles@gmail.com>
2022-03-03 10:39:39 -0600

    finished second bonus on grades

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -37,24 +37,51 @@ public class GradesApplication {
         students.put("MrMundst", student3);
         students.put("TheBeard", student4);
 
-        System.out.println("-----Here is a list of the student's GitHub usernames-----");
-        for (String key : students.keySet()) {
-            System.out.printf("%s\n", key);
-        }
-        System.out.println("Which student would you like to see more information on?");
-        String choice = sc.nextLine();
-        Student userChoice = students.get(choice);
 
-        try{
-            System.out.printf("Github name: %s%n"
-            + "Real name: %s%n"
-            + "Current grades: %s%n"
-            + "Current average: %.1f%n ",
-                    choice, userChoice.getName(), userChoice.getGrades(), userChoice.getGradeAverage()
-            );
-        }catch (Exception e){
-            System.out.println("Sorry, that user does not exist...");
-        }
 
+        System.out.println("What would you like to do?");
+        System.out.println();
+        System.out.println("0. Exit application");
+        System.out.println("1. View a student's information");
+        System.out.println(("2. View all of the students grades."));
+
+
+        int menuChoice = 0;
+        boolean cont = true;
+
+        while (cont) {
+            menuChoice = sc.nextInt();
+            switch (menuChoice) {
+                case 0:
+                    cont = false;
+                    System.out.println("Thank you for using the grading application");
+                case 1:
+                    System.out.println("-----Here is a list of the student's GitHub usernames-----");
+                    System.out.println();
+                    for (String key : students.keySet()) {
+                        System.out.printf("|%s|", key);
+                    }
+                    System.out.println();
+                    System.out.println("\nWhich student would you like to see more information on?");
+                    String choice = sc.nextLine();
+                    Student userChoice = students.get(choice);
+                    try {
+                        System.out.printf("Github name: %s%n"
+                                        + "Real name: %s%n"
+                                        + "Current grades: %s%n"
+                                        + "Current average: %.1f%n ",
+                                choice, userChoice.getName(), userChoice.getGrades(), userChoice.getGradeAverage()
+                        );
+                    } catch (Exception e) {
+                        System.out.println("Sorry, that user does not exist...");
+                    }
+                case 2:
+                    for (String student : students.keySet()){
+                        System.out.printf("Name %s%n Grades: %s%n", students.get(student).getName(), students.get(student).getGrades());
+                    }
+
+
+            }
+        }
     }
 }
