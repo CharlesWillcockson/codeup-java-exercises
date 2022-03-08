4267cb7dfd3293119b7ec5cc6d7c75b4a30a7fe9
Charles Willcockson <willcockson.charles@gmail.com>
2022-03-08 13:59:47 -0600

    starting contacts manager

---
+++ src/ContactsManager/Contacts.java
@@ -0,0 +1,13 @@
+package ContactsManager;
+
+public class Contacts {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Contacts(String firstName, String lastName, String phoneNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+}
--- src/inOutPractice/IOPractice.java
+++ src/inOutPractice/IOPractice.java
@@ -1,14 +1,18 @@
 package inOutPractice;
 import java.nio.file.*;
 import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 
 public class IOPractice {
-    String directory = "data";
-    String file = "data.txt";
-
-    List<String> groceryList = Arrays.asList("coffee", "milk", "sugar");
-    Path filepath = Paths.get("data", "groceries.txt");
-    Files.write(filepath, groceryList);
+    public static void main(String[] args) throws IOException {
+        try ( DataOutputStream dout =
+                      new DataOutputStream(new FileOutputStream("data.txt")) ) {
+            dout.writeDouble(1.1);
+            dout.writeInt(55);
+            dout.writeBoolean(true);
+            dout.writeChar('4');
+        }
+    }
 }
