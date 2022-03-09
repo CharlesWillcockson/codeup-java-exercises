e28dc8d3659565b1b7446969181fa35234c2e288
Charles Willcockson <willcockson.charles@gmail.com>
2022-03-09 08:57:12 -0600

    practice for assessment

--- src/JavaPractice/Review.java
+++ src/JavaPractice/Review.java
@@ -1,4 +1,33 @@
 package JavaPractice;
 
 public class Review {
+    private double bill;
+    private double tipPercentage;
+
+    public static double total(double bill, double tipPercentage) {
+        double total;
+        total = bill + ((tipPercentage/100) * bill);
+        return total;
+    }
+
+    private static int[] nums = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    public static void even(int[] nums){
+        int[] evens;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] % 2 == 0){
+                System.out.println(nums[i]);
+            }
+
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("total(20.00, 10.0) = " + total(20.00, 10.0));
+        even(nums);
+    }
 }
+
--- src/practiceFromW3.java
+++ src/practiceFromW3.java
@@ -4,8 +4,22 @@
 import java.util.Scanner;
 
 public class practiceFromW3 {
-    public static void main(String[] args) {
+    String str;
+    public static void characterAt (String str){
+        int index1 = str.charAt(0);
+        int index2 = str.charAt(10);
+        System.out.println((char)index1);
+        System.out.println((char)index2);
+    }
 
-//        System.out.println("Hello\nCharles Willcockson");
+    public static double smallest(double a, double b, double c){
+        return Math.min(a, Math.min(b,c));
+    }
+
+
+
+    public static void main(String[] args) {
+        characterAt("Java exercises!");
+        System.out.println("smallest(25.1, 32.2, 1.4) = " + smallest(25.1, 32.2, 1.4));
     }
 }
