7c016b7bccb4eb64c69ba4ec694d37142dced963
Charles Willcockson <willcockson.charles@gmail.com>
2022-03-13 20:37:11 -0500

    codewars return negative

---
+++ src/CodeWars/ReturnNegative.java
@@ -0,0 +1,14 @@
+package CodeWars;
+
+public class ReturnNegative {
+    public static int makeNegative(final int x) {
+        if (x > 0) {
+            return x * -1;
+        }
+        return x
+    }
+
+    public static void main(String[] args) {
+        System.out.println(makeNegative(-232));
+    }
+}
