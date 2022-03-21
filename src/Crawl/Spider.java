fe7f74bffdf796495b485470efd94fba02a2f4bf
Charles Willcockson <willcockson.charles@gmail.com>
2022-03-21 10:48:45 -0500

    starting web crawler

---
+++ src/Crawl/Spider.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+package Crawl;
+
+public class Spider {
+    private static final int MaxPagesToSearch = 10;
+    private Set<String> pagesVisited = new HashSet<String>();
+    private List<String> pagesToVisit = new LinkedList<String>();
+
+    private String nextURL(){
+        String nextURL;
+        do{
+            nextURL = this.pagesToVisit.remove(0);
+        } while (this.pagesVisited.contains(nextURL));
+        this.pagesVisited.add(nextURL);
+        return nextURL;
+    }
+
+    public void search(String url, String searchWord){
+        while (this.pagesVisited.size() < MaxPagesToSearch){
+            String currentURL;
+            SpiderLeg leg = new SpiderLeg();
+            if (this.pagesToVisit.isEmpty()){
+                currentURL = url;
+                this.pagesVisited.add(url);
+            }else{
+                currentURL = this.nextURL();
+            }
+            leg.crawl(currentURL);
+            boolean success = leg.searchForWord(searchWord);
+            if (success){
+                System.out.println(String.format("**Success** Word %s found at %s", searchWord, currentUrl));
+                break;
+            }
+            this.pagesToVisit.addAll(leg.getLinks());
+        }
+        System.out.println(String.format("**Done** Visited %s web page(s)", this.pagesVisited.size());
+    }
+}
