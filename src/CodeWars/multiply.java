a639c87022b152d6aa3a44be4732f08c0317bf4f
Charles Willcockson <willcockson.charles@gmail.com>
2022-03-23 21:43:57 -0500

    codewars kata finished

---
+++ src/CodeWars/multiply.java
@@ -0,0 +1,12 @@
+package CodeWars;
+
+public class multiply {
+    public static int mult(int num1, int num2){
+        return num1*num2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("mult(2,3) = " + mult(2, 3));
+    }
+}
+
